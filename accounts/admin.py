9b9c67c1d1dcd171747d8efb674c17e930079bcc
Johair Alam <johairalam@gmail.com>
2026-03-18 21:34:41 +0300

    Sync settings and UserAdmin for permissions

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,25 +1,13 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
 
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-                'groups',
-                'user_permissions',  # this is the important line
-            )
-        }),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    fieldsets = BaseUserAdmin.fieldsets + (
         ('Role', {'fields': ('role',)}),
     )
 
