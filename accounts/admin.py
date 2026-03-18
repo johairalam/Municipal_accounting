457d7644f1e62510f6a47822f196c55e82a989a5
Johair Alam <johairalam@gmail.com>
2026-03-18 20:51:49 +0300

    Show user_permissions in UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,12 +3,23 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
 
-    fieldsets = BaseUserAdmin.fieldsets + (
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',  # this is the important line
+            )
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Role', {'fields': ('role',)}),
     )
 
